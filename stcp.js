1dab87aac464ec9738233afc927b4179d5e72fd3
clinis <dinisareias@gmail.com>
2016-03-03 06:48:11 +0000

    added notifications and other fixes
    - added notifications when arrival time is less than x minutes;
    - fixed the bug of buses not having arrival time and thus showing
    unidentified
    - added verification of correct usage of the arguments

--- stcp.js
+++ stcp.js
@@ -5,8 +5,15 @@
 
 var request = require('request');
 var cheerio = require('cheerio');
+const notifier = require('node-notifier');
 
-var url = 'http://www.stcp.pt/pt/itinerarium/soapclient.php?codigo='; url += process.argv[2]; //example usage: node stcp IPO5
+var busStation = process.argv[2];
+if(busStation == null){
+    console.log('example usage: node stcp IPO5');
+}
+
+var url = 'http://www.stcp.pt/pt/itinerarium/soapclient.php?codigo='+busStation;
+
 
 request(url, (function(err, resp, body) {
     if (err)
@@ -16,6 +23,28 @@ request(url, (function(err, resp, body) {
         event = $(this).text().trim().replace(/\s\s+/g, ',').split(',');
         //console.log(event[0] + ': ' + event[1]);
         //console.log(event);
-        console.log(event[0] + ': ' + event[1] + ' ' + event[2] + ' ' + event[3]);
+        if(event[3] == null){
+            console.log(event[0] + ': ' + event[1] + ' ' + event[2] + ' 0');
+            notifier.notify({
+              title: 'You just missed '+event[0],
+              message: event[0]+' now passing in '+busStation,
+              sound: true,
+              wait: false
+            }, function(error, response) {
+              console.log(response);
+            });
+        }else{
+            console.log(event[0] + ': ' + event[1] + ' ' + event[2] + ' ' + event[3]);
+            if(event[3].match(/\d/g) <= 10){
+                notifier.notify({
+                  title: event[3]+' to '+event[0],
+                  message: 'at '+busStation,
+                  sound: true,
+                  wait: false
+                }, function(error, response) {
+                  console.log(response);
+                });
+            }
+        }
     });
 }))
