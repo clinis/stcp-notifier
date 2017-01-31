83e0b6d58e169649eccae39423acb70bf7dea7bc
Dinis <dinisareias@gmail.com>
2017-01-31 21:44:00 +0000

    print results in a table
    Print results in a centered table (requires blessed). Exit the app with
    “q”, “ESC” or “CTRL+C”.

--- stcp.js
+++ stcp.js
@@ -4,11 +4,16 @@ const program = require('commander');
 var request = require('request');
 var cheerio = require('cheerio');
 const notifier = require('node-notifier');
+var blessed = require('blessed');
 
+var stationLines = [];
+var infos = [];
+
+// Programm argumments and commands
 program
   .version('0.0.2')
   .description('Get the remaining times of the next buses at a bus stop using that bus stop code.')
-  .usage('<bus stop code>')
+  .usage('<bus stop code>. Example: stcp IPO5')
   .arguments('<busStopCode>')
   .option('-l, --line <lineNumber>','See only buses of a certain line. Example: 205')
   .action(function (busStopCode) {
@@ -16,6 +21,39 @@ program
   });
 program.parse(process.argv);
 
+// Create a screen object.
+var screen = blessed.screen();
+
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
+
+// Create a centered table
+var box = blessed.table({
+  top: 'center',
+  left: 'center',
+  width: '50%',
+  height: '50%',
+  border: {
+    type: 'line'
+  },
+  style: {
+    border: {
+      fg: '#f0f0f0'
+    }
+  }
+});
+
+// Create a centered text area
+var list = blessed.text({
+  top: 'center',
+  left: 'center',
+  width: '50%',
+  height: '50%',
+
+  align: 'left'
+});
 
 function getTime() {
     var date = new Date();
@@ -29,9 +67,27 @@ function getTime() {
     return hour + ":" + min;
 }
 
+function getLinesOfStation (station) {
+  var url = 'http://www.stcp.pt/pt/itinerarium/callservice.php?action=srchstoplines&stopname='+station;
+
+  request(url, function(err, resp, body) {
+    if (err){
+      throw err;
+    }
+
+    var json = JSON.parse(body);
+
+    if(json[0] !== undefined){
+      for (var x = 0; x < json[0].lines.length; x++) {
+        stationLines.push(json[0].lines[x].code);
+      }
+    }
+  });
+}
+
 function processTheInfo(parsedInfo){
   if(parsedInfo[3] == null){
-    console.log(parsedInfo[0] + ': ' + parsedInfo[1] + ' ' + parsedInfo[2]);
+    //console.log(parsedInfo[0] + ': ' + parsedInfo[1] + ' ' + parsedInfo[2]);
     notifier.notify({
       title: 'You just missed '+parsedInfo[0],
       message: parsedInfo[0]+' now passing in '+station,
@@ -39,7 +95,7 @@ function processTheInfo(parsedInfo){
       wait: false
     });
   }else{
-    console.log(parsedInfo[0] + ': ' + parsedInfo[1] + ' ' + parsedInfo[2] + ' ' + parsedInfo[3]);
+    //console.log(parsedInfo[0] + ': ' + parsedInfo[1] + ' ' + parsedInfo[2] + ' ' + parsedInfo[3]);
     if(parsedInfo[3].match(/\d+/g) <= 10){
       notifier.notify({
         title: parsedInfo[3]+' to '+parsedInfo[0],
@@ -56,64 +112,56 @@ function req() {
     if (err){
       throw err;
     }
+    infos = [];
 
     $ = cheerio.load(body);
 
     var erros = $('.msgBox span').text();
 
-    if ( erros.substring(0,17) == "Nao ha autocarros" ) {
+    if ( erros.substring(0,17) == "Nao ha autocarros" ) {         // got no buses warning
       console.log("------ " +getTime()+ " ------");
       console.log("There are no buses on the next 60 minutes.");
-    } else if ( erros.substring(0,18) == "Por favor, utilize" ) {
+    } else if ( erros.substring(0,18) == "Por favor, utilize" ) { // got wrong station code warning
       console.log("Please, enter a valid bus stop code.");
-      process.exit()
-    } else {
+    } else {                                                      // got no warnings
       console.log("------ " +getTime()+ " ------");
-
+      infos.push(["Linha","Destino","Horas","Tempo"]);
       var parsed = $('#smsBusResults .even');
-      if (parsed.length > 0) {
+      if (parsed.length > 0) {                                    // if found any time result
         parsed.each(function() {
           parsedInfo = $(this).text().trim().replace(/\s\s+/g, ',').split(',');
+          infos.push(parsedInfo);
           processTheInfo(parsedInfo);
         });
-      } else {
-        getLinesOfStation(station);
+        box.setData(infos);
+        screen.append(box);
+      } else {                                                  // if didn't found any time result
+        if (stationLines.indexOf(line) > -1) {
+          //console.log("There are no buses on the next 60 minutes");
+          list.setText("There are no buses on the next 60 minutes");
+        } else {
+          //console.log("Line "+line+" doesn't pass on "+station);
+          //console.log("Here are the lines that pass on "+station+":");
+          //console.log(stationLines);
+          list.setText("Line "+line+" doesn't pass on "+station+".\
+          Here are the lines that pass on "+station+":\
+          "+stationLines.toString() );
+          //process.exit();
+        }
+        screen.append(list);
       }
     }
+    screen.render();
   });
 }
 
-function getLinesOfStation (station) {
-  var stationLines = [];
-  var url = 'http://www.stcp.pt/pt/itinerarium/callservice.php?action=srchstoplines&stopname='+station;
-
-  request(url, function(err, resp, body) {
-    if (err){
-      throw err;
-    }
-
-    var json = JSON.parse(body);
-
-    for (var x = 0; x < json[0].lines.length; x++) {
-      stationLines.push(json[0].lines[x].code);
-    }
-
-    if (stationLines.indexOf(line) > -1) {
-      console.log("There are no buses on the next 60 minutes");
-    } else {
-      console.log("Line "+line+" doesn't pass on "+station);
-      console.log("Here are the lines that pass on "+station+":");
-      console.log(stationLines);
-      process.exit();
-    }
-  });
-}
-
-if(!program.args.length) {
-  program.help();
-} else {
+if(!program.args.length) {  // if no arguments passed
+  program.help();           // print help
+} else {                    // else, normal program
   var line = (program.line === undefined) ? 0 : program.line;
   var url = 'http://www.stcp.pt/itinerarium/soapclient.php?codigo='+station+'&linha='+line;
+  getLinesOfStation(station);
+
   req();
-  setInterval(req, 30000); // 30000
+  setInterval(req, 30000); // 30000ms = 30s
 }
