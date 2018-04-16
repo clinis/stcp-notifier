ad3190923429c5b3ffef99dc828b9f6c73f4d566
Dinis <dinisareias@gmail.com>
2018-04-16 21:39:56 +0100

    request function with promises

--- stcp.js
+++ stcp.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const request = require('request');
+const rp = require("request-promise-native");
 const cheerio = require('cheerio');
 const notifier = require('node-notifier');
 const blessed = require('blessed');
@@ -11,12 +11,12 @@ const path = require('path');
 const HTMLOffline = "<div id=\"smsBusHeader\"><div class=\"filtro\"><a href=\"http://www.stcp.pt/smsBusMicroSite/index.html\" target=\"_blank\"><img border=\"0\" src=\"/temas/stcp/imgs/logo-smsbus.jpg\" /></a><form action=\"\" id=\"frmFiltro\"><label for=\"linhasmsbus\">Filtar por linha</label><input type=\"hidden\" name=\"paragem\" value=\"aal1\" /><input type=\"hidden\" name=\"t\" value=\"smsbus\" /><select id=\"linhasmsbus\" name=\"linha\" onchange=\"javascript: frmFiltro.submit();\"><option value=\"0\">---</option><option value=\"3M\">3M </option><option value=\"4M\">4M </option><option value=\"5M\">5M </option></select></form></div><div class=\"clear\"></div></div><table id=\"smsBusResults\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\"><tr><th>Linha</th><th>Hora Prevista</th><th>Tempo de Espera</th></tr><tr class=\"even\"><td><ul class=\"linhasAssoc\"><li><a target=\"_self\" class=\"linha_5m\" title=\"\" href=\"/pt/viajar/linhas/?linha=5M \">5M </a></li></ul>&nbsp;ERMESINDE(ES</td><td><i>00:58</i></td><td>1min</td></tr><tr class=\"even\"><td><ul class=\"linhasAssoc\"><li><a target=\"_self\" class=\"linha_4m\" title=\"\" href=\"/pt/viajar/linhas/?linha=4M \">4M </a></li></ul>&nbsp;AV. ALIADOS</td><td><i>01:39</i></td><td>42min</td></tr><tr class=\"even\"><td><ul class=\"linhasAssoc\"><li><a target=\"_self\" class=\"linha_3m\" title=\"\" href=\"/pt/viajar/linhas/?linha=3M \">3M </a></li></ul>&nbsp;AV. ALIADOS</td><td><i>01:50</i></td><td>53min</td></tr></table>"
 const HTMLAPassar = "<div id=\"smsBusHeader\"><div class=\"filtro\"><a href=\"http://www.stcp.pt/smsBusMicroSite/index.html\" target=\"_blank\"><img border=\"0\" src=\"/temas/stcp/imgs/logo-smsbus.jpg\" /></a><form action=\"\" id=\"frmFiltro\"><label for=\"linhasmsbus\">Filtar por linha</label><input type=\"hidden\" name=\"paragem\" value=\"srpt1\" /><input type=\"hidden\" name=\"t\" value=\"smsbus\" /><select id=\"linhasmsbus\" name=\"linha\" onchange=\"javascript: frmFiltro.submit();\"><option value=\"0\">---</option><option value=\"508\">508 </option><option value=\"602\">602 </option></select></form></div><div class=\"clear\"></div></div><table id=\"smsBusResults\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\"><tr><th>Linha</th><th>Hora Prevista</th><th>Tempo de Espera</th></tr><tr class=\"even\"><td><ul class=\"linhasAssoc\"><li><a target=\"_self\" class=\"linha_602\" title=\"\" href=\"/pt/viajar/linhas/?linha=602 \">602 </a></li></ul>&nbsp;CC.VIVACCI-F</td><td><i>a passar</i></td><td></td></tr><tr class=\"even\"><td><ul class=\"linhasAssoc\"><li><a target=\"_self\" class=\"linha_508\" title=\"\" href=\"/pt/viajar/linhas/?linha=508 \">508 </a></li></ul>&nbsp;FREIXIEIRO -</td><td><i>00:24</i></td><td>15min</td></tr></table>";
 
-var stationLines = [];
-var runStation;
+let runStation;
 
 // Programm argumments and commands
+// TODO: add option to set the time window to get notifications
 program
-    .version('0.0.3')
+    .version('0.0.4')
     .description('Get the remaining times of the next buses at a bus stop using that bus stop code.')
     .usage('<bus stop code>. Example: stcp IPO5')
     .arguments('<busStopCode>')
@@ -26,47 +26,53 @@ program
     });
 program.parse(process.argv);
 
-// // Create a screen object.
-// var screen = blessed.screen();
-//
-// // Quit on Escape, q, or Control-C.
-// screen.key(['escape', 'q', 'C-c'], function (ch, key) {
-//     return process.exit(0);
-// });
-//
-// // Create a centered table
-// var box = blessed.table({
-//     top: 'center',
-//     left: 'center',
-//     width: '50%',
-//     height: '50%',
-//     border: {
-//         type: 'line'
-//     },
-//     style: {
-//         border: {
-//             fg: '#f0f0f0'
-//         }
-//     }
-// });
-//
-// // Create a centered text area
-// var list = blessed.text({
-//     top: 'center',
-//     left: 'center',
-//     width: '50%',
-//     height: '50%',
-//
-//     align: 'left'
-// });
-//
-// // Create a top left text area
-// var reqtimetext = blessed.text({
-//     top: '0',
-//     left: '0',
-//
-//     align: 'left'
-// });
+// Create a screen object.
+let screen = blessed.screen();
+
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q', 'C-c'], function (ch, key) {
+    return process.exit(0);
+});
+
+// Create a centered table
+let table = blessed.table({
+    top: 'center',
+    left: 'center',
+    width: '50%',
+    height: '50%',
+    border: {
+        type: 'line'
+    },
+    style: {
+        border: {
+            fg: '#f0f0f0'
+        }
+    }
+});
+
+// Create a centered text area
+let list = blessed.text({
+    top: 'center',
+    left: 'center',
+    width: '50%',
+    height: '50%',
+
+    align: 'left'
+});
+
+// Create a top left text area
+let reqtimetext = blessed.text({
+    top: '0',
+    left: '0',
+
+    align: 'left'
+});
+
+
+screen.append(reqtimetext);
+screen.append(table);
+screen.append(list);
+
 
 function getTime() {
     let date = new Date();
@@ -80,96 +86,11 @@ function getTime() {
     return hour + ':' + min;
 }
 
-function getLinesOfStation(station) {
-    let url = 'http://www.stcp.pt/pt/itinerarium/callservice.php?action=srchstoplines&stopname=' + station;
-
-    request(url, function (err, resp, body) {
-        if (err) {
-            throw err;
-        }
-
-        let json = JSON.parse(body);
-
-        if (json[0] !== undefined) {
-            for (let x = 0; x < json[0].lines.length; x++) {
-                stationLines.push(json[0].lines[x].code);
-            }
-        }
-    });
-}
-
-function processTheInfo(parsedInfo) {
-    if (parsedInfo[3] == null) {
-        notifier.notify({
-            title: 'You just missed ' + parsedInfo[0],
-            message: parsedInfo[0] + ' now passing in ' + runStation,
-            sound: true,
-            wait: false
-        });
-    } else {
-        if (parsedInfo[3].match(/\d+/g) <= 10) {
-            notifier.notify({
-                title: parsedInfo[3] + ' to ' + parsedInfo[0],
-                message: 'at ' + runStation,
-                sound: true,
-                wait: false
-            });
-        }
-    }
-}
-
-function req(station, line) {
-    let url = 'http://www.stcp.pt/itinerarium/soapclient.php?codigo=' + station + '&linha=' + line;
-
-    request(url, function (err, resp, body) {
-        if (err) {
-            throw err;
-        }
-        infos = [];
-
-        $ = cheerio.load(body);
-        screen.append(reqtimetext);
-
-        let erros = $('.msgBox span').text();
-
-        if (erros.substring(0, 17) === 'Nao ha autocarros') {         // got no buses warning
-            reqtimetext.setText('Last request: ' + getTime());
-            reqtimetext.pushLine('There are no buses on the next 60 minutes.');
-        } else if (erros.substring(0, 18) === 'Por favor, utilize') { // got wrong station code warning
-            list.setText('Please, enter a valid bus stop code.');
-            clearInterval(intervalID);
-            reqtimetext.setText('Timmer stopped. You can quit the app now.');
-            screen.append(list);
-        } else {                                                      // got no warnings
-            reqtimetext.setText('Last request: ' + getTime());
-
-            infos.push(['Linha', 'Destino', 'Horas', 'Tempo']);
-            var parsed = $('#smsBusResults .even');
-            if (parsed.length > 0) {                                    // if found any time result
-                parsed.each(function () {
-                    parsedInfo = $(this).text().trim().replace(/\s\s+/g, ',').split(',');
-                    infos.push(parsedInfo);
-                    processTheInfo(parsedInfo);
-                });
-                box.setData(infos);
-                screen.append(box);
-            } else {                                                  // if didn't found any time result
-                if (stationLines.indexOf(line) > -1) {
-                    list.setText('There are no buses on the next 60 minutes');
-                } else {
-                    list.setText('Line ' + line + ' doesn\'t pass on ' + station + '.\n\n Here are the lines that pass on ' + station + ':\n ' + stationLines.toString());
-                    clearInterval(intervalID);
-                    reqtimetext.setText('Timmer stopped. You can quit the app now.');
-                }
-                screen.append(list);
-            }
-        }
-        screen.render();
-    });
+function print(info) {
+    console.log(info.line + ": " + info.hours + " (" + info.time + ")");
 }
 
 function notify(info) {
-
     if (info.time === '') {
         notifier.notify({
             title: "You just missed " + info.line,
@@ -189,66 +110,79 @@ function notify(info) {
     }
 }
 
+function processResults(results) {
+    let tableText = [["Linha", "Horas", "Tempo"]];
 
-function parseResults(body) {
-    let bodyParsed = [];
-    bodyParsed.results = [];
+    if (results.warnings.includes('Nao ha autocarros previstos para a paragem')) {
+        // console.log("não há autocarros");
+        // console.log(results.warnings);
+        reqtimetext.pushLine('There are no buses on the next 60 minutes.');
 
-    let $ = cheerio.load(body);
-
-    bodyParsed.warnings = $('.msgBox span').text();
-
-    $('#smsBusResults .even').each(function () {
-        bodyParsed.results.push({
-            line: $(this).find('a').text().trim(),
-            //destination: $(this).find('td:first-child').text(),       // TODO: parse destination
-            hours: $(this).find('i').text(),
-            time: $(this).find('td:last-child').text()
-        })
-    });
-
-    return bodyParsed;
-}
-
-function updateInfos(time, parsed) {
-
-    if (parsed.warnings.includes('Nao ha autocarros previstos para a paragem')) {
-        console.log("não há autocarros");
-        console.log(parsed.warnings);
-    } else if (parsed.warnings.includes('Por favor, utilize o codigo SMSBUS')) {
-        console.log("por favor utilize");
-        console.log(parsed.warnings);
+    } else if (results.warnings.includes('Por favor, utilize o codigo SMSBUS')) {
+        // console.log("por favor utilize codigo certo");
+        // console.log(results.warnings);
+        list.setText('Please, enter a valid bus stop code.');
+        clearInterval(intervalID);
+        reqtimetext.setText('Timmer stopped. You can quit the app now.');
     } else {
-        console.log("update e console.log..........");
-        for (let l of parsed.results) {
-            console.log(l.line + ": " + l.hours + " (" + l.time + ")");
-
+        //console.log("update e console.log..........");
+        reqtimetext.setText('Last request: ' + getTime());
+        for (let l of results.results) {
+            tableText.push([l.line, l.hours, l.time]);
             notify(l);
         }
+        table.setData(tableText);
+        table.setFront();
     }
+    screen.render();
 }
 
-function req2(station, line) {
-    let url = 'http://www.stcp.pt/itinerarium/soapclient.php?codigo=' + station + '&linha=' + line;
-
-    request(url, function (err, resp, body) {
-        if (err) {
-            throw err;
+function req(station, line){
+    let options = {
+        uri: "http://www.stcp.pt/itinerarium/soapclient.php?codigo=" + station,
+        transform: function (body) {
+            return cheerio.load(body);
         }
+    };
 
-        let reqTime = getTime();
+    rp(options)
+        .then(function ($) {
+            // Process the html
 
-        updateInfos(reqTime, parseResults(body));
-    });
+            reqtimetext.setText('Last request: ' + getTime());
+
+            let bodyParsed = [];
+            bodyParsed.warnings = [];
+            bodyParsed.results = [];
+
+            bodyParsed.warnings = $('.msgBox span').text();
+
+            $('#smsBusResults .even').each(function () {
+                bodyParsed.results.push({
+                    line: $(this).find('a').text().trim(),
+                    //destination: $(this).find('td:first-child').text(),    // TODO: parse destination
+                    hours: $(this).find('i').text(),
+                    time: $(this).find('td:last-child').text()
+                })
+            });
+
+            //console.log(bodyParsed);
+            processResults(bodyParsed);
+        })
+        .catch(function (err) {
+            // Crawling failed or Cheerio choked...
+            console.log("ERROR: ", err);
+        });
 }
+
 
 if (!program.args.length) {  // if no arguments passed
-    program.help();           // print help
-} else {                    // else, normal program
-    let runLine = (program.line === undefined) ? 0 : program.line.toUpperCase();
+    program.help();          // print help
+} else {                     // else, normal program
+    let runLine = (program.line === undefined) ? 0 : program.line.toUpperCase();    // TODO: add line filter
 
-    //getLinesOfStation(station);
+    //getLinesOfStation(station);    // TODO: add verification that line passes at station
 
-    req2(runStation, runLine);
-    //var intervalID = setInterval(req, 30000); // 30000ms = 30s
+    req(runStation, runLine);
+    var intervalID = setInterval(req, 30000, runStation); // 30000ms = 30s
 }
