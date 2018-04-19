71d112d882e71d55fca629fa8e63779e0613c319
Dinis <dinisareias@gmail.com>
2018-04-19 22:55:24 +0100

    change messages to PT

--- stcp.js
+++ stcp.js
@@ -7,9 +7,6 @@ const notifier = require('node-notifier');
 const blessed = require('blessed');
 const path = require('path');
 
-// <<<---- TEMPLATE HTML ------>>>>>>> \\
-const HTMLOffline = "<div id=\"smsBusHeader\"><div class=\"filtro\"><a href=\"http://www.stcp.pt/smsBusMicroSite/index.html\" target=\"_blank\"><img border=\"0\" src=\"/temas/stcp/imgs/logo-smsbus.jpg\" /></a><form action=\"\" id=\"frmFiltro\"><label for=\"linhasmsbus\">Filtar por linha</label><input type=\"hidden\" name=\"paragem\" value=\"aal1\" /><input type=\"hidden\" name=\"t\" value=\"smsbus\" /><select id=\"linhasmsbus\" name=\"linha\" onchange=\"javascript: frmFiltro.submit();\"><option value=\"0\">---</option><option value=\"3M\">3M </option><option value=\"4M\">4M </option><option value=\"5M\">5M </option></select></form></div><div class=\"clear\"></div></div><table id=\"smsBusResults\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\"><tr><th>Linha</th><th>Hora Prevista</th><th>Tempo de Espera</th></tr><tr class=\"even\"><td><ul class=\"linhasAssoc\"><li><a target=\"_self\" class=\"linha_5m\" title=\"\" href=\"/pt/viajar/linhas/?linha=5M \">5M </a></li></ul>&nbsp;ERMESINDE(ES</td><td><i>00:58</i></td><td>1min</td></tr><tr class=\"even\"><td><ul class=\"linhasAssoc\"><li><a target=\"_self\" class=\"linha_4m\" title=\"\" href=\"/pt/viajar/linhas/?linha=4M \">4M </a></li></ul>&nbsp;AV. ALIADOS</td><td><i>01:39</i></td><td>42min</td></tr><tr class=\"even\"><td><ul class=\"linhasAssoc\"><li><a target=\"_self\" class=\"linha_3m\" title=\"\" href=\"/pt/viajar/linhas/?linha=3M \">3M </a></li></ul>&nbsp;AV. ALIADOS</td><td><i>01:50</i></td><td>53min</td></tr></table>"
-const HTMLAPassar = "<div id=\"smsBusHeader\"><div class=\"filtro\"><a href=\"http://www.stcp.pt/smsBusMicroSite/index.html\" target=\"_blank\"><img border=\"0\" src=\"/temas/stcp/imgs/logo-smsbus.jpg\" /></a><form action=\"\" id=\"frmFiltro\"><label for=\"linhasmsbus\">Filtar por linha</label><input type=\"hidden\" name=\"paragem\" value=\"srpt1\" /><input type=\"hidden\" name=\"t\" value=\"smsbus\" /><select id=\"linhasmsbus\" name=\"linha\" onchange=\"javascript: frmFiltro.submit();\"><option value=\"0\">---</option><option value=\"508\">508 </option><option value=\"602\">602 </option></select></form></div><div class=\"clear\"></div></div><table id=\"smsBusResults\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\"><tr><th>Linha</th><th>Hora Prevista</th><th>Tempo de Espera</th></tr><tr class=\"even\"><td><ul class=\"linhasAssoc\"><li><a target=\"_self\" class=\"linha_602\" title=\"\" href=\"/pt/viajar/linhas/?linha=602 \">602 </a></li></ul>&nbsp;CC.VIVACCI-F</td><td><i>a passar</i></td><td></td></tr><tr class=\"even\"><td><ul class=\"linhasAssoc\"><li><a target=\"_self\" class=\"linha_508\" title=\"\" href=\"/pt/viajar/linhas/?linha=508 \">508 </a></li></ul>&nbsp;FREIXIEIRO -</td><td><i>00:24</i></td><td>15min</td></tr></table>";
 
 let runStation;
 let runLine;
@@ -18,11 +15,11 @@ let runMinETA;
 // Programm argumments and commands
 program
     .version('0.0.4')
-    .description('Get the remaining times of the next buses at a bus stop using that bus stop code.')
-    .usage('<bus stop code>. Example: stcp IPO5')
+    .description('Tempos de chegada dos próximos autocarros da STCP numa certa paragem através do código SMS BUS dessa paragem.')
+    .usage('<código SMS BUS>. Por exemplo: stcp IPO5')
     .arguments('<busStopCode>')
-    .option('-l, --line <lineNumber>', 'See only buses of a certain line. Example: 205')
-    .option('-t, --eta <minutes>', 'Set the minutes of ETA that trigger notifications.', parseInt)
+    .option('-l, --line <lineNumber>', 'Filtrar só para uma linha. Por exemplo: 205')
+    .option('-t, --eta <minutes>', 'Definir o número de minutos a partir do qual mostra notificações. Pré-definido em 10 minutos.', parseInt)
     .action(function (busStopCode) {
         runStation = busStopCode.toUpperCase();
         runLine = (program.line === undefined) ? 0 : program.line;
@@ -122,16 +119,16 @@ function print(info) {
 function notify(info) {
     if (info.time === '') {
         notifier.notify({
-            title: "You just missed " + info.line,
-            message: info.line + " is now passing in " + runStation,
+            title: "Acabaste de perder o " + info.line + "!",
+            message: info.line + " está agora a passar em " + runStation,
             sound: true,
             icon: path.join(__dirname, 'STCP.png'),
             timeout: 10
         })
     } else if (info.time.match(/\d+/g) <= runMinETA) {
         notifier.notify({
-            title: info.time + ' to ' + info.line,
-            message: 'at ' + runStation,
+            title: info.time + ' para o ' + info.line,
+            message: 'em ' + runStation,
             sound: true,
             icon: path.join(__dirname, 'STCP.png'),
             timeout: 5
@@ -145,18 +142,18 @@ function processResults(results) {
     if (results.warnings.includes('Nao ha autocarros previstos para a paragem')) {   // got no buses warning
         // console.log("não há autocarros");
         // console.log(results.warnings);
-        reqtimetext.pushLine('There are no buses on the next 60 minutes.');
+        reqtimetext.pushLine('Nao há resultados para a paragem nos próximos 60 minutos.');
 
     } else if (results.warnings.includes('Por favor, utilize o codigo SMSBUS')) {   // got wrong station code warning
         // console.log("por favor utilize codigo certo");
         // console.log(results.warnings);
-        list.setText('Please, enter a valid bus stop code.');
+        list.setText('Por favor, insere um código SMS BUS de paragem válido.');
         clearInterval(intervalID);
-        reqtimetext.setText('Timmer stopped. You can quit the app now.');
+        reqtimetext.setText('Execução terminada. Podes sair.');
     } else {                                                                       // got no warnings but...
         if (results.results.length > 0){                                              // found time results
             //console.log("update e console.log..........");
-            reqtimetext.setText('Last request: ' + getTime());
+            reqtimetext.setText('Última actualização: ' + getTime());
             for (let l of results.results) {
                 tableText.push([l.line, l.hours, l.time]);
                 notify(l);
@@ -165,11 +162,11 @@ function processResults(results) {
             table.setFront();
         } else {                                                                      // didn't found any time result
             if (stationLines.indexOf(runLine) > -1) {
-                list.setText("There are no buses on the next 60 minutes");
+                list.setText("Nao há resultados para a paragem nos próximos 60 minutos.");
             } else {
-                list.setText("Line "+runLine+" doesn't pass on "+runStation+".\n\nHere are the lines that pass on "+runStation+":\n"+stationLines);
+                list.setText("A linha "+runLine+" não passa em "+runStation+".\n\nAs linhas que passam em "+runStation+" são:\n"+stationLines);
                 clearInterval(intervalID);
-                reqtimetext.setText("Timmer stopped. You can quit the app now.");
+                reqtimetext.setText("Execução terminada. Podes sair.");
             }
         }
     }
@@ -188,7 +185,7 @@ function req(station, line){
         .then(function ($) {
             // Process the html
 
-            reqtimetext.setText('Last request: ' + getTime());
+            reqtimetext.setText('Última actualização: ' + getTime());
 
             let bodyParsed = [];
             bodyParsed.warnings = [];
